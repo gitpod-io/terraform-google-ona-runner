ad5d740156d9c74932ab5ae3724b7b24c474f712
gitpod-next-automation <gitpod-next-automation@gitpod.io>
2026-05-19 17:20:37 +0000

    Update GCP runner, proxy, prometheus, and node-exporter images to version 20260519.1032
    This PR updates the runner, proxy, prometheus, and node-exporter image references in locals.tf to use the latest images:
    - Runner: us-docker.pkg.dev/gitpod-next-production/gitpod-next/gitpod-gcp-runner:20260519.1032
    - Proxy: us-docker.pkg.dev/gitpod-next-production/gitpod-next/gitpod-proxy:20260519.1032
    - Prometheus: us-docker.pkg.dev/gitpod-next-production/gitpod-next/prometheus:v3.11.3
    - Node-exporter: us-docker.pkg.dev/gitpod-next-production/gitpod-next/node-exporter:v1.11.1
    
    Auto-generated by: https://github.com/gitpod-io/gitpod-next/actions/runs/26112910467

--- locals.tf
+++ locals.tf
@@ -18,8 +18,8 @@ locals {
   })
 
   # Default images
-  default_runner_image = "us-docker.pkg.dev/gitpod-next-production/gitpod-next/gitpod-gcp-runner:20260519.882"
-  default_proxy_image  = "us-docker.pkg.dev/gitpod-next-production/gitpod-next/gitpod-proxy:20260519.882"
+  default_runner_image = "us-docker.pkg.dev/gitpod-next-production/gitpod-next/gitpod-gcp-runner:20260519.1032"
+  default_proxy_image  = "us-docker.pkg.dev/gitpod-next-production/gitpod-next/gitpod-proxy:20260519.1032"
 
   default_prometheus_image    = "us-docker.pkg.dev/gitpod-next-production/gitpod-next/prometheus:v3.11.3"
   default_node_exporter_image = "us-docker.pkg.dev/gitpod-next-production/gitpod-next/node-exporter:v1.11.1"
